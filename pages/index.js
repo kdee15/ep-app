65e408de615b25b3a6e56bd38fe0cd0658419219
Kirk Daniels <kirkdaniels@me.com>
2021-08-22 15:31:41 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
-import { getAllEpisodes } from "../dummy-data";
+import ArrowRightIcon from "../components/icons/arrow-right-icon";
+import Button from "../components/ui/button";
+import { useState, useEffect } from "react";
 import EpisodeList from "../components/episodes/episode-list";
 
 function HomePage() {
-  const allEpisodes = getAllEpisodes();
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedEpisodes, setLoadedEpisodes] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const episodes = [];
+        for (const episode in data._embedded.episodes) {
+          episodes.push(data._embedded.episodes[episode]);
+        }
+        setIsLoading(false);
+        setLoadedEpisodes(episodes);
+      });
+  }, []);
+
+  if (isLoading) {
+    <section>
+      <p>Loading ...</p>
+    </section>;
+  }
+
   return (
-    <section class="container">
-      <h1>Welcome to the next project</h1>
-      <EpisodeList items={allEpisodes} />
+    <section>
+      <h1>All Episodes Page</h1>
+      <EpisodeList episodes={loadedEpisodes} />
     </section>
   );
 }
